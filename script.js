5dd544d6d9e085bbc00e47c39f808f895542b655
Reyben <somoherbenne09@gmail.com>
2023-09-29 11:28:12 +0800

    added auto input effect

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ hamburger.onclick = function() {
     navbar = document.querySelector(".nav-bar")
     navbar.classList.toggle("active")
 }
+
+var typed = new Typed(".auto-input", {
+        strings: ["Herbenne Rey", "Web Developer", "Programmer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true 
+})
